11708a7c3bbb0b2fd399ba3e4c3e2df13424d964
Luis Cordoba <luchitocordoba.lc@gmail.com>
2022-04-19 13:13:04 -0300

    Cambio de baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/project-root/public/';
+    public $baseURL = 'https://baseunica.sectc.app/';
 
     /**
      * --------------------------------------------------------------------------
